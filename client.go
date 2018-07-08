4bec264d2c919f08a4a1ae79fe64e91c6eda12e9
Carlos C <uldericofilho@gmail.com>
2018-07-08 13:16:52 +0200

    dynamolock: split documentation into a separate file

--- client.go
+++ client.go
@@ -13,72 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-
-// Package dynamolock provides a simple utility for using DynamoDB's consistent
-// read/write feature to use it for managing distributed locks.
-//
-// In order to use this package, the client must create a table in DynamoDB,
-// although the client provides a convenience method for creating that table
-// (CreateTable).
-//
-// Basic usage:
-//
-//	import (
-//		"log"
-//
-//		"cirello.io/dynamolock"
-//		"github.com/aws/aws-sdk-go/aws"
-//		"github.com/aws/aws-sdk-go/aws/session"
-//		"github.com/aws/aws-sdk-go/service/dynamodb"
-//	)
-//
-//	// ---
-//
-//	svc := dynamodb.New(session.New(), &aws.Config{
-//		Region:   aws.String("us-west-2"),
-//	})
-//	c, err := dynamolock.New(svc,
-//		"locks",
-//		dynamolock.WithLeaseDuration(3*time.Second),
-//		dynamolock.WithHeartbeatPeriod(1*time.Second),
-//	)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//
-//	log.Println("ensuring table exists")
-//	c.CreateTable("locks",
-//		&dynamodb.ProvisionedThroughput{
-//			ReadCapacityUnits:  aws.Int64(5),
-//			WriteCapacityUnits: aws.Int64(5),
-//		},
-//		dynamolock.WithCustomPartitionKeyName("key"),
-//	)
-//
-//	data := []byte("some content a")
-//	lockedItem, err := c.AcquireLock("spock",
-//		dynamolock.WithData(data),
-//		dynamolock.ReplaceData(),
-//	)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//
-//	log.Println("lock content:", string(lockedItem.Data()))
-//	if got := string(lockedItem.Data()); string(data) != got {
-//		log.Println("losing information inside lock storage, wanted:", string(data), " got:", got)
-//	}
-//
-//	log.Println("cleaning lock")
-//	success, err := c.ReleaseLock(lockedItem)
-//	if !success {
-//		log.Fatal("lost lock before release")
-//	}
-//	if err != nil {
-//		log.Fatal("error releasing lock:", err)
-//	}
-//	log.Println("done")
-package dynamolock // import "cirello.io/dynamolock"
+package dynamolock
 
 import (
 	"context"
