c3edae7b2838d2d92e73291bdccd47e9eaf3bc5c
U Cirello <ulderico.cirello@gmail.com>
2024-06-09 21:59:18 -0700

    dynamolock/v2: improve test conclusion flakiness

--- v2/client_sort_key_test.go
+++ v2/client_sort_key_test.go
@@ -651,18 +651,22 @@ func TestSortKeyClientClose(t *testing.T) {
 func TestSortKeyInvalidReleases(t *testing.T) {
 	t.Parallel()
 	svc := dynamodb.NewFromConfig(defaultConfig(t))
+	logger := &bufferedContextLogger{}
 	c, err := dynamolock.New(svc,
 		sortKeyTable,
 		dynamolock.WithLeaseDuration(3*time.Second),
 		dynamolock.WithHeartbeatPeriod(1*time.Second),
 		dynamolock.WithOwnerName("TestInvalidReleases#1"),
-		dynamolock.WithLogger(&testLogger{t: t}),
+		dynamolock.WithLogger(logger),
 		dynamolock.WithPartitionKeyName("key"),
 		dynamolock.WithSortKey("sortkey", "sortvalue"),
 	)
 	if err != nil {
 		t.Fatal(err)
 	}
+	t.Cleanup(func() {
+		t.Log(logger.String())
+	})
 
 	t.Log("ensuring table exists")
 	_, _ = createSortKeyTable(t, c)
